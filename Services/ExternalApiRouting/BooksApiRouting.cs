6439502a84765d31121247fe4fd8adb356fc0a2d
elvis <elvissosaortiz27@gmail.com>
2025-02-11 13:45:19 -0400

    feat: Create HttpClient and the model for the external api

---
+++ Services/ExternalApiRouting/BooksApiRouting.cs
@@ -0,0 +1,8 @@
+﻿namespace Services.ExternalApiRouting
+{
+    public class BooksApiRouting
+    {
+        public const string Root = "/api/v1/Books";
+        public static string ManageBooks(int id) => $"{Root}/{id}";
+    }
+}
---
+++ Services/Models/BooksModel.cs
@@ -0,0 +1,37 @@
+﻿namespace Services.Models
+{
+    using Newtonsoft.Json;
+    using System.Text.Json.Serialization;
+
+    public class BooksModel
+    {
+        public IEnumerable<Book> Books { get; set; } = [];
+
+        public class Book
+        {
+            [JsonProperty("id")]
+            [JsonPropertyName("id")]
+            public int Id { get; set; }
+
+            [JsonProperty("title")]
+            [JsonPropertyName("title")]
+            public string Title { get; set; } = null!;
+
+            [JsonProperty("description")]
+            [JsonPropertyName("description")]
+            public string Description { get; set; } = null!;
+
+            [JsonProperty("pageCount")]
+            [JsonPropertyName("pageCount")]
+            public int PageCount { get; set; }
+
+            [JsonProperty("excerpt")]
+            [JsonPropertyName("excerpt")]
+            public string Excerpt { get; set; } = null!;
+
+            [JsonProperty("publishDate")]
+            [JsonPropertyName("publishDate")]
+            public DateTime PublishDate { get; set; }
+        }
+    }
+}
