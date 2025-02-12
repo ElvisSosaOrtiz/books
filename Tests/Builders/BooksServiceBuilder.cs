f66e71dbd1d8f44826c7c8f34225b5794bea9577
elvis <elvissosaortiz27@gmail.com>
2025-02-12 13:55:30 -0400

    test: Create builder for service instantiation

---
+++ Tests/Builders/BooksServiceBuilder.cs
@@ -0,0 +1,33 @@
+﻿namespace Tests.Builders
+{
+    using Microsoft.Extensions.Logging;
+    using Moq;
+    using Services;
+    using System.Net.Http;
+
+    public class BooksServiceBuilder
+    {
+        private IHttpClientFactory? _httpClientFactory;
+        private ILogger<BooksService>? _logger;
+
+        public BooksService Build()
+        {
+            var httpClientFactory = _httpClientFactory ?? Mock.Of<IHttpClientFactory>();
+            var logger = _logger ?? Mock.Of<ILogger<BooksService>>();
+
+            return new BooksService(httpClientFactory, logger);
+        }
+
+        public BooksServiceBuilder With(IHttpClientFactory value)
+        {
+            _httpClientFactory = value;
+            return this;
+        }
+
+        public BooksServiceBuilder With(ILogger<BooksService> value)
+        {
+            _logger = value;
+            return this;
+        }
+    }
+}
